0c1aa3bf2e67cac5d20ba71c2ddd90b9a72693a7
pr4ject4ne-code <pr4ject4ne@gmail.com>
2026-09-11 22:27:17 +0100

    Add files via upload

--- src/app/emergency-numbers/page.tsx
+++ src/app/emergency-numbers/page.tsx
@@ -1,1 +1,12 @@
+import Layout from '@/components/Layout';
+import EmergencyNumbers from '@/components/EmergencyNumbers';
 
+export const metadata = { title: 'Emergency Numbers | Racoon Eye' };
+
+export default function EmergencyNumbersPage() {
+  return (
+    <Layout page="emergency-numbers">
+      <EmergencyNumbers />
+    </Layout>
+  );
+}
